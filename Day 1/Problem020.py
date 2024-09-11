2d5f3e9c133f239e09e015e3d8ddc693d18e2b0f
Nilstam428 <nileshtamboli428@gmail.com>
2024-09-11 21:28:33 +0530

    Problem 20 solve

--- Day 1/Problem020.py
+++ Day 1/Problem020.py
@@ -1,9 +1,10 @@
 # give even number up to n
 
 number = int(input("Enter the number: "))
-li=[]
-for i in range (2,number+1):
-  if((number+1)/2):
-    li.append
+for i in range(2,number+1,2):
+  print(i, end=",")
   
-print(li)    
+#method 2
+for j in range(2,number+1):
+  if(j%2==0):   
+    print(j, end=",")
