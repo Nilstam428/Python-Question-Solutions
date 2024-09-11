baa010682d6a4b44b223884cbd0de9d2be6a271e
Nilstam428 <nileshtamboli428@gmail.com>
2024-09-11 19:15:33 +0530

    update in repo

--- Day 1/Problem019.py
+++ Day 1/Problem019.py
@@ -1,1 +1,10 @@
 # Q. number of unique combination on two dice
+
+#for getting number of unique combination on two dice we can perform loop for getting values
+
+count = 0
+for i in range(1, 7):
+    for j in range(1, 7):
+        count += 1
+
+print("Number of unique combinations:", count) 
--- Day 1/Problem020.py
+++ Day 1/Problem020.py
@@ -1,1 +1,9 @@
 # give even number up to n
+
+number = int(input("Enter the number: "))
+li=[]
+for i in range (2,number+1):
+  if((number+1)/2):
+    li.append
+  
+print(li)    
