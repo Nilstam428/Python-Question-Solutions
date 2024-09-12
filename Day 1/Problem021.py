843da306dc94aba4ce412fc1e25ca9826961b532
Nilstam428 <nileshtamboli428@gmail.com>
2024-09-12 16:45:49 +0530

    Update

--- Day 1/Problem021.py
+++ Day 1/Problem021.py
@@ -1,1 +1,6 @@
 # how to know that element in list start with given charchter
+
+# my_list = ["apple", "banana", "cherry", "date", "elderberry"]
+# char = "a"
+# result = [element for element in my_list if element.startswith(char)]
+# print(result)
