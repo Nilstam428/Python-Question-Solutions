878e616f76865cffcd1ac6b6e777163ffde40dee
Nilstam428 <98279958+Nilstam428@users.noreply.github.com>
2024-09-15 17:07:48 +0530

    Update probelm1.py

--- Day 003/probelm1.py
+++ Day 003/probelm1.py
@@ -1,5 +1,22 @@
 # 1. name = "yourName"
-
 # 2. function => "python"
 # 3. function => odd / even
 # 4. function => table print
+# Table printing
+
+
+name="Nilesh Tamboli"
+
+def get_table(num):
+  num = int(input("Enter a number"))
+  for i in range(1,11):
+     print(f"{num}x{i}={num*i}")
+
+# Even odd
+def even_odd(n):
+  n = int(input("Enter a number"))
+    if(n%2==0):
+        print(f"{n} is even !!")
+    else:
+        print(f"{n} odd number!!")
+print(even_odd(n))
